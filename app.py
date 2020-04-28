25d26b7c5c8e4e3a1e5e1449b3e02b275d1f1299
countzr <countzr@gmail.com>
2020-04-28 19:50:55 +0200

    working app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+world = '🗺️'
+
+def hello(word=world):
+    return "Hello " + word
+
+
+@app.route('/')
+def index():
+    return hello()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
